c59b72f2c41d218000b881b7099f18beaa2fd655
yh72167 <yh.19972372@gmail.com>
2021-02-12 02:13:09 +0900

    [add]旧アプリのHTML・CSSへの差し替え。初期設定の変更など

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,23 +1,373 @@
-@extends('layouts.app')
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <title>タイトル</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css" integrity="sha512-+4zCK9k+qNFUR5X+cKL9EIR+ZOhtIloNl9GIKS57V1MyNsYpYcUrUeQc9vNfzsWfV28IaLL3i96P9sdNyeRssA==" crossorigin="anonymous" />
+    <!-- Styles -->
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <script src="https://code.jquery.com/jquery-3.3.1.js"></script>
+    <script src="./js/app.js"></script>
+</head>
 
-@section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">Dashboard</div>
+<body>
+<!--ヘッダー読み込み-->
 
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
 
-                    You are logged in!
+    <!-- ヘッダー関係 -->
+    <header id="index-top" class="header js-toggle-sp-menu-target">
+        <div class="header__content-wrap">
+        <!-- タイトル -->
+        <h1 class="header__title" href="index.php"><a href="index.php" class="header__title-link">EmRev</a></h1>
+        </div>
+    </header>
+
+    <div id="js-show-msg" class="msg-slide">
+    </div>
+
+    <!-- メニューバーの内容部分 -->
+    <nav class="menuAbout">
+        <ul class="menuAbout__itemWrap">
+        <li class="menuAbout__itemWrap-item"><a class="menuLink-color" href="./mypage.php">マイページ</a></li>
+        <li class="menuAbout__itemWrap-item">お気に入りレビュー一覧</li>
+        <li class="menuAbout__itemWrap-item"><a class="menuLink-color" href="./revliReviewList.php">投稿されたレビュー一覧</a></li>
+        <li class="menuAbout__itemWrap-item"><a class="menuLink-color" href="./browsingHistory.php">閲覧履歴</li>
+        <li class="menuAbout__itemWrap-item"><a class="menuLink-color" href="./employeeRegistration.php">投稿者登録</a></li>
+        <li class="menuAbout__itemWrap-item">登録社員一覧</li>
+        <li class="menuAbout__itemWrap-item"><a class="menuLink-color" href="./passwordReminder.php">パスワード変更</a></li>
+        <li class="menuAbout__itemWrap-item"><a class="menuLink-color" href="./reviewCompanyRegistration.php">レビュー会社登録申請</li>
+        <li class="menuAbout__itemWrap-item"><a class="menuLink-color" href="./withdrawal.php">退会する</a></li>
+        </ul>
+    </nav>
+
+<!-- ヒーローバナー -->
+<section class="hero">
+
+<!-- テキスト関係 -->
+    <div class="hero__content">
+
+    <div class="hero__text-wrap">
+        <h1 class="hero__text-catchTheam">
+        Easier Deployment
+        </h1>
+        <div class="hero__text-about">
+        サンプルサンプルサンプルサンプルサンプルサンプルサンプルサンプル
+        サンプルサンプルサンプルサンプルサンプルサンプルサンプルサン
+        </div>
+        <a class="hero__text-aboutLink" href="#index-about">このアプリについて</a>
+    </div>
+
+    <div class="hero__signup-loginWrap">
+        <!-- 会員登録関係 -->
+        <div class="hero__signup js-signup-style">
+
+        <form action="" method="post" class="hero__signup-formStyle">
+            <h2 class="hero__signup-title">SignUp</h2>
+            <div class="hero__signup-commonMsgArea">
+                <!-- 接続エラー等のメッセージをここに出力させる。 -->
+                <!--例外処理発生時に出力されるメッセージを出す処理-->
+
+            </div>
+
+            <!-- メールアドレス入力欄 -->
+            <div class="hero__signup-emailaddressField">
+            <!-- 後にphpでエラー時用のスタイルを付属させる様にする。 -->
+
+            <label class="#">
+                <!-- バリに引っかかった際には$err_msに関連するvalueが入るので、それを判定元にerrクラスを付属させる。 -->
+                <!-- value内は入力記録の保持 -->
+                <input class="hero__signup-emailForm">
+                <!-- 後にphpでエラーメッセージを出力させる様にする。-->
+                <div class="hero__signup-areaMsg">
+                </div>
+            </label>
+            </div>
+
+            <!-- パスワード入力 -->
+            <div class="hero__signup-passwardField">
+            <label class="#">
+                <!-- 後にphpでエラー時用のスタイルを付属させる様にする。 -->
+                <input class="hero__signup-passwordForm"">
+                <div class="hero__signup-areaMsg">
+
+                </div>
+            </label>
+            </div>
+
+            <!-- 確認用パスワード入力 -->
+            <div class="hero__signup-confirmationPasswardField">
+            <!-- 後にphpでエラー時用のスタイルを付属させる様にする。 -->
+            <label class="#">
+                <input class="hero__signup-passwordConfirmationForm" name="password_re" type="password" placeholder="Confirmation Password" value="">
+            </label>
+            <div class="hero__signup-areaMsg">
+            </div>
+            </div>
+
+            <div class="hero__signup-registerBtnField">
+            <input class="hero__signup-registerBtn" type="submit" name="user_register" value="登録する">
+            </div>
+
+        </form>
+        </div>
+
+
+        <!-- ログイン関係 -->
+        <div class="hero__login js-login-style hidden">
+
+        <form action="" method="post" class="hero__login-formStyle">
+            <h2 class="hero__login-title">Login</h2>
+                <div class="hero__login-commonMsgArea">
+                <!-- 接続エラー等のメッセージをここに出力させる。 -->
+                <!--例外処理発生時に出力されるメッセージを出す処理-->
+
+                </div>
+
+            <!-- メールアドレス入力欄 -->
+            <div class="hero__login-emailaddressField">
+                <!-- 後にphpでエラー時用のスタイルを付属させる様にする。 -->
+
+                <label class="#">
+                    <!-- バリに引っかかった際には$err_msに関連するvalueが入るので、それを判定元にerrクラスを付属させる。 -->
+                    <!-- value内は入力記録の保持 -->
+                    <input class="hero__login-emailForm" type="text" name="login-email" placeholder="Email" value="">
+                    <!-- 後にphpでエラーメッセージを出力させる様にする。-->
+                    <div class="hero__login-areaMsg">
+                    </div>
+                </label>
+            </div>
+
+            <!-- パスワード入力 -->
+            <div class="hero__login-passwardField">
+                <!-- 後にphpでエラー時用のスタイルを付属させる様にする。 -->
+                <input class="hero__login-passwordForm" type="password" name="login-pass" placeholder="Password" value="">
+                <div class="hero__login-areaMsg">
                 </div>
             </div>
+
+            <div class="hero__login-registerBtnField">
+                <input class="hero__login-registerBtn" type="submit" name="user_login" value="ログイン">
+            </div>
+        </form>
+
         </div>
+
+    </div>
     </div>
 </div>
-@endsection
+</section>
+
+<!-- 新着レビューコンテンツ -->
+<section class="review">
+
+<div class="review__content-block">
+    <h3 class="review__content-title">New reviews</h3>
+    <!-- 新着レビューコンテンツ(今は仮レイアウトの都合上複数同じ要素を作っているがphpを書き始めた際にはfor文で回す) -->
+    <div class="review__content-wrap">
+    <div class="review__content-individual">
+
+        <!-- イメージ画像 -->
+        <img class="review__image" rel="#">
+
+        <!-- ユーザー名 -->
+        <div href="#" class="review__userName-style">
+        <span class="review__userName">
+            ユーザー名
+        </span>
+        </div>
+
+        <!-- 会社名 -->
+        <div href="#" class="review__companyName-style">
+        <span class="review__companyName">会社名</span>
+        </div>
+
+        <!-- 会社の業界 -->
+        <div href="#" class="review__industry-style">
+        <span class="review__industry">会社の業界</span>
+        </div>
+
+        <!-- レビュー内容(総評) -->
+        <div class="review__generalComment-style">
+        <div class="review__generalComment">
+            サンプルサンプルサンプル<br>
+            サンプルサンプルサンプル<br>
+            サンプルサンプルサンプル<br>
+            サンプルサンプルサンプル<br>
+            サンプルサンプルサンプル...
+        </div>
+        </div>
+
+        <!-- 詳細を見る -->
+        <div href="#" class="review__detail-style">
+        <span class="review__detail">
+        詳細を見る
+        </span>
+        </div>
+    </div>
+
+    <!-- レビューコンテンツ(今は仮レイアウトの都合上複数同じ要素を作っているがphpを書き始めた際にはfor文で回す) -->
+    <div class="review__content-individual">
+
+        <!-- イメージ画像 -->
+        <img class="review__image" rel="#">
+
+        <!-- ユーザー名 -->
+        <div href="#" class="review__username-style">
+        <span class="review__userName">
+            ユーザー名
+        </span>
+        </div>
+
+        <!-- 会社名 -->
+        <div href="#" class="review__companyName-style">
+        <span class="review__companyName">会社名</span>
+        </div>
+
+        <!-- 会社の業界 -->
+        <div href="#" class="review__industry-style">
+        <span class="review__industry">会社の業界</span>
+        </div>
+
+        <!-- レビュー内容(総評) -->
+        <div class="review__generalComment-style">
+        <div class="review__generalComment">
+            サンプルサンプルサンプル<br>
+            サンプルサンプルサンプル<br>
+            サンプルサンプルサンプル<br>
+            サンプルサンプルサンプル<br>
+            サンプルサンプルサンプル...
+        </div>
+        </div>
+
+        <!-- 詳細を見る -->
+        <div href="#" class="review__detail-style">
+        <span class="review__detail">
+        詳細を見る
+        </span>
+        </div>
+    </div>
+
+    <!-- レビューコンテンツ(今は仮レイアウトの都合上複数同じ要素を作っているがphpを書き始めた際にはfor文で回す) -->
+    <div class="review__content-individual">
+
+        <!-- イメージ画像 -->
+        <img class="review__image" rel="#">
+
+        <!-- ユーザー名 -->
+        <div href="#" class="review__userName-style">
+        <span class="review__userName">
+            ユーザー名
+        </span>
+        </div>
+
+        <!-- 会社名 -->
+        <div href="#" class="review__companyName-style">
+        <span class="review__companyName">会社名</span>
+        </div>
+
+        <!-- 会社の業界 -->
+        <div href="#" class="review__industry-style">
+        <span class="review__industry">会社の業界</span>
+        </div>
+
+        <!-- レビュー内容(総評) -->
+        <div class="review__generalComment-style">
+        <div class="review__generalComment">
+            サンプルサンプルサンプル<br>
+            サンプルサンプルサンプル<br>
+            サンプルサンプルサンプル<br>
+            サンプルサンプルサンプル<br>
+            サンプルサンプルサンプル...
+        </div>
+        </div>
+
+        <!-- 詳細を見る -->
+        <div href="#" class="review__detail-style">
+        <span class="review__detail">
+        詳細を見る
+        </span>
+        </div>
+    </div>
+    </div>
+</div>
+</section>
+
+
+<!-- このアプリについて -->
+<section id="index-about" class="about">
+<div class="about__content-wrap">
+    <h3 class="about__content-title">
+    About app
+    </h3>
+    <div class="about__content-text">
+    サンプルサンプルサンプルサンプルサンプルサンプルサンプル
+    サンプルサンプルサンプルサンプルサンプルサンプルサンプル
+    サンプルサンプルサンプルサンプルサンプルサンプルサンプル
+    サンプルサンプルサンプルサンプルサンプルサンプルサンプル
+    サンプルサンプルサンプルサンプルサンプルサンプルサンプル
+    サンプルサンプルサンプルサンプルサンプルサンプルサンプル
+    サンプルサンプルサンプルサンプルサンプルサンプルサンプル
+    サンプルサンプルサンプルサンプルサンプルサンプルサンプル
+    サンプルサンプルサンプルサンプルサンプルサンプルサンプル
+    サンプルサンプルサンプルサンプルサンプルサンプルサンプル
+    サンプルサンプルサンプルサンプルサンプルサンプルサンプル
+    サンプルサンプルサンプルサンプルサンプルサンプルサンプル
+    サンプルサンプルサンプルサンプルサンプルサンプルサンプル
+    サンプルサンプルサンプルサンプルサンプルサンプルサンプル
+    サンプルサンプルサンプルサンプルサンプルサンプルサンプル
+    サンプルサンプルサンプルサンプルサンプルサンプルサンプル
+    サンプルサンプルサンプルサンプルサンプルサンプルサンプル
+    サンプルサンプルサンプルサンプルサンプルサンプルサンプル
+    サンプルサンプルサンプルサンプルサンプルサンプルサンプル
+    サンプルサンプルサンプルサンプルサンプルサンプルサンプル
+    サンプルサンプルサンプルサンプルサンプルサンプルサンプル
+    サンプルサンプルサンプルサンプルサンプルサンプルサンプル
+    サンプルサンプルサンプルサンプルサンプルサンプルサンプル
+    サンプルサンプルサンプルサンプルサンプルサンプルサンプル
+    </div>
+    <!-- リンク先はセッション内のログイン情報に沿ってif文で変更する。ログインしている場合はマイページへ。していない場合はサインアップ画面へ移動する。-->
+    <a href="#index-top" class="about__content-link active-signup-menu">
+    このアプリを使ってみる
+    </a>
+</div>
+</section>
+
+<!-- お問い合わせフォーム -->
+<section class="contact" id="index-contact">
+<div class="contact__content-wrap">
+    <div class="contact__content-title">
+    CONTACT
+    </div>
+    <div class="contact__content-body">
+    <from action="" class="">
+        <input class="contact__content-form" type="text" placeholder="お名前">
+        <input class="contact__content-form" type="email" placeholder="E-Mail">
+        <input class="contact__content-form" placeholder="お問い合わせの種類">
+        <textarea class="contact__content-areaForm" placeholder="お問い合わせ内容"></textarea>
+        <button class="contact__content-buttom">送信する</button>
+    </from>
+    </div>
+</div>
+</section>
+
+    <!-- フッター -->
+    <footer class="footer">
+        <div class="footer__element-wrap">
+        <div class="footer__element-copyright">
+            <h1 class="footer__element-copyrightTitle">EmRev</h1>
+            Copyright © Y.H<br>All Rights Reserved
+        </div>
+        <div class="footer__element-link">
+            sample<br>
+            sample<br>
+            sample
+        </div>
+        <div class="footer__element-sns">
+            sample<br>
+            sample<br>
+            sample
+        </div>
+        </div>
+    </footer>
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -1,50 +1,14 @@
 <?php
 
-// RouteクラスとAuthクラスは名前空間での指定がいらない。
-// https://kohkimakimoto.hatenablog.com/entry/2014/01/17/153750
-
-// public function auth(array $options = [])
-// {
-//     // Authentication Routes...
-//     $this->get('login', 'Auth\LoginController@showLoginForm')->name('login');
-//     $this->post('login', 'Auth\LoginController@login');
-//     $this->post('logout', 'Auth\LoginController@logout')->name('logout');
-
-//     // Registration Routes...
-//     if ($options['register'] ?? true) {
-//         $this->get('register', 'Auth\RegisterController@showRegistrationForm')->name('register');
-//         $this->post('register', 'Auth\RegisterController@register');
-//     }
-
-//     // Password Reset Routes...
-//     if ($options['reset'] ?? true) {
-//         $this->resetPassword();
-//     }
-
-//     // Email Verification Routes...
-//     if ($options['verify'] ?? false) {
-//         $this->emailVerification();
-//     }
-// }
-
-
-// 認証系の物の物を一括で行う。
-//vendor/laravel/framework/src/Illuminate/Routing/Router.phpで
-//定義されたルーティングが読み込まれている。
 Auth::routes();
 
 use App\Http\Controllers\DrillsController;
 
 Route::get('/', function () {
-    return view('child');
+    return view('home');
 });
 
 Route::get('/drills/new','DrillsController@new')->name('drills.new');
 Route::post('/drills','DrillsController@create')->name('drills.create');
-Route::get('/drills','DrillsController@index')->name('drills');
-Route::get('/drills/{id}/edit','DrillsController@edit')->name('drills.edit');
-Route::post('/drills/{id}','DrillsController@update')->name('drills.update');
-Route::post('/drills/{id}/delete','DrillsController@destroy')->name('drills.delete');
-Route::get('/drills/{id}','DrillsController@show')->name('drills.show');
 
 Auth::routes();
