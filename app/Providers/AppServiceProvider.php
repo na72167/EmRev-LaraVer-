a0a779391f985f71b3b522e682d3bd16c6bdac13
yh72167 <yh.19972372@gmail.com>
2021-02-16 23:51:49 +0900

    [wip]認証機能

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -13,7 +13,11 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        //
+        if(config('app.env') !== 'production'){
+            \DB::listen(function ($query){
+                \Log::info("Query Time:{$query->time}s] $query->sql");
+            });
+        }
     }
 
     /**
--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,7 @@
 
 use App\Http\Controllers\RegisterController;
 use App\Http\Controllers\LoginController;
-use App\Http\Controllers\Auth\ResetPasswordController;
+use App\Http\Controllers\ResetPasswordController;
 
 Route::get('/', function () {
     return view('home');
@@ -10,11 +10,15 @@ Route::get('/', function () {
 
 // ========================認証系========================
 Route::post('/register','RegisterController@register')->name('register');
+Route::get('/logout','LoginController@logout');
 Route::post('/login','LoginController@login')->name('login');
-Route::get('/logout','LoginController@logout')->name('logout');
+Route::get('/passwordReset', function () {
+    return view('/password/passwordReminder');
+});
+Route::post('/passwordReset','ResetPasswordController@resetPassword')->name('passwordReset');
+Route::get('/passRemindReceive', function () {
+    return view('/password/passRemindReceive');
+});
 Route::get('/myPage/{id}', function () {
     return view('myPage');
-});
-Route::get('/passwordReminder', function () {
-    return view('/password/passwordReminder');
 });
