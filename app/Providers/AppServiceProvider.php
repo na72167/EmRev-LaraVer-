52073dfdee92fd479fd5d7762eda7e1ff645bc4b
yh72167 <yh.19972372@gmail.com>
2021-02-23 20:29:47 +0900

    [wip]認証機能

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,7 +2,8 @@
 
 namespace App\Providers;
 
-use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Gate;
+use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -21,12 +22,36 @@ class AppServiceProvider extends ServiceProvider
     }
 
     /**
+     * The policy mappings for the application.
+     *
+     * @var array
+     */
+    protected $policies = [
+        'App\Model' => 'App\Policies\ModelPolicy',
+    ];
+
+    /**
      * Bootstrap any application services.
      *
      * @return void
      */
     public function boot()
     {
-        //
+
+        $this->registerPolicies();
+
+    // 管理者のみ許可
+    Gate::define('admin-only', function ($user) {
+        return ($user->role == 1);
+    });
+
+    // 投稿者登録済みアカウントに許可
+    Gate::define('contributor-status', function ($user) {
+        return ($user->role == 50);
+    });
+    // 一般ユーザにのみ許可
+    Gate::define('general-status', function ($user) {
+        return ($user->role == 100);
+    });
     }
 }
